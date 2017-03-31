98f35b4ef9369b68202ebf56753db52c15ce24fa
Michał Makaruk <makaruk.michal@gmail.com>
2017-03-31 16:28:08 +0200

    added static gif

--- src/main/java/pl/akademiakodu/controller/GifController.java
+++ src/main/java/pl/akademiakodu/controller/GifController.java
@@ -1,7 +1,9 @@
 package pl.akademiakodu.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.GetMapping;
+import pl.akademiakodu.controller.model.Gif;
 
 @Controller
 public class GifController {
@@ -10,5 +12,14 @@ public class GifController {
     public String listGifs() {
         return "home";
     }
+
+
+    @GetMapping("/gif")
+    public String gifDetails(ModelMap modelMap){
+        Gif gif = new Gif("cowboy-coder","michalos",true);
+        modelMap.addAttribute("gif",gif);
+        return "gif-details";
+    }
+
 
 }
